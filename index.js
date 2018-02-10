ff741b8634ceed373ca86c89b7f01c752577d20f
Alain Armand <aarmand.inbox@gmail.com>
2018-02-10 13:38:05 +0100

    clean comments

--- index.js
+++ index.js
@@ -44,14 +44,3 @@ async function run() {
 }
 
 run();
-// async function run() {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-
-//   await page.goto("https://github.com");
-//   await page.screenshot({ path: "screenshots/github.png" });
-
-//   browser.close();
-// }
-
-// run();
