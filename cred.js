2eab9b1e14fd1429363851a39bcc6cedcb36d535
Alain Armand <aarmand.inbox@gmail.com>
2018-02-10 12:50:23 +0100

    add env code

---
+++ cred.js
@@ -0,0 +1,8 @@
+const env = require("env2")("./.env");
+
+module.exports = {
+  username: process.env.GITHUB_USERNAME,
+  password: process.env.GITHUB_PASSWORD
+};
+
+console.log(process.env.GITHUB_USERNAME);
